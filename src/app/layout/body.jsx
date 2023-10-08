d421d966404200ae0e022587f928a8cb78fa771f
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-10-08 17:00:05 -0300

    Main Body layout

--- src/app/layout/body.jsx
+++ src/app/layout/body.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Main from './Main'
 
 const Body = () => {
   return (
-    <div className='flex-1 h-screen bg-softblue dark:bg-[#121212]'>
+    <div className='flex-1 h-screen bg-softblue dark:bg-[#444654]'>
       <Navbar />
+      <Main />
     </div>
   )
 }
